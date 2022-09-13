42d2233f4f05d6a0b4ce6b87209b08431156f3ef
manohar092003 <113410928+manohar092003@users.noreply.github.com>
2022-09-13 10:47:24 +0530

    Add files via upload

---
+++ tictactoe.py
@@ -0,0 +1,16 @@
+# Function to print the Tic-Tac-Toe Design  
+def mytictactoe(val):  
+    print("\n")  
+    print("\t     |     |")  
+    print("\t  {}  |  {}  |  {}".format(val[0], val[1], val[2]))  
+    print('\t_____|_____|_____')  
+   
+    print("\t     |     |")  
+    print("\t  {}  |  {}  |  {}".format(val[3], val[4], val[5]))  
+    print('\t_____|_____|_____')  
+   
+    print("\t     |     |")  
+   
+    print("\t  {}  |  {}  |  {}".format(val[6], val[7], val[8]))  
+    print("\t     |     |")  
+    print("\n")
